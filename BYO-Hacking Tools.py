f29a793149ad6122429b45f992fe5a7a8759ba3a
steven20022 <35538211+steven20022@users.noreply.github.com>
2023-02-05 23:39:08 -0500

    Added NetCat.py

--- BYO-Hacking Tools.py
+++ BYO-Hacking Tools.py
@@ -1,17 +1,21 @@
+import os
 import socket
 import threading
 import subprocess
 
 def menu():
+    
     selector = -1
     while(selector != 0):
-        selector = input('Pick a Tool\n1. TCP Client Creator\n2. TCP Server\n0. Exit\n')
+        selector = input('Pick a Tool\n1. TCP Client\n2. TCP Server\n3. Replacing Netcat\n0. Exit\n')
         if(selector.isnumeric):
             match selector:
                 case '1':
                     tcp_client()
                 case '2':
                     tcp_server()
+                case '3':
+                    replacing_netcat()
                 case '0':
                     quit()
         else:
@@ -19,27 +23,71 @@ def menu():
 
 
 def tcp_client():
-    target_host = input("Please enter target_host: ")
-    target_port = int(input("Please enter target_port: "))
+    try:
+        target_host = "127.0.0.1" #"my.chc.edu" #socket.gethostbyname(input("Please enter target_host: "))
+    except socket.gaierror:
+        print("Error resolving host")
+        menu()
+        exit()
+    target_port = 9090 #int(input("Please enter target_port: "))
 
     # create a socket object
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket successfully created")
+    except socket.error as err:
+        print("socket creation failed %s" %(err))
+        menu()
+        exit()
 
     # connect the client
-    client.connect((target_host, target_port))
-
+    try:
+        client.connect((target_host,target_port))
+        print("connected")
+    except socket.error as err:
+        print('Error: ',err)
+        menu()
+        exit()
+    
     # send some data
-    client.send(input("Enter data to sent: ").encode())
+    client.send("Hello Server!".encode()) #client.send(input("Enter data to sent: ").encode())
+    print("Data Sent")
 
     # receive some data
-    response = client.recv(100)
+    response = client.recv(1024)
 
-    print("Server Response: \n",response.decode())
-    client.close()
+    print(response)
+    client.close()  
 
 def tcp_server():
-    IP = '0.0.0.0'
-    PORT = 9998
+    IP = input("Please enter IP: ")
+    PORT = int(input("Please enter PORT: "))
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((IP, PORT))
+    server.listen()
+    print(f'[*] Listening on {IP}:{PORT}')
+    def handle_client(client_socket):
+        with client_socket as sock:
+            request = sock.recv(1024)
+            print(f'[*] Received: {request.decode()}')
+            sock.send('Hello Client'.encode())
+    while True:
+        try:
+            client, address = server.accept()
+            print("accepted")
+        except KeyboardInterrupt:
+            break
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        # client_handler = threading.Thread(target=handle_client, args=(client,))
+        # client_handler.start()
+        handle_client(client)
+        
+def replacing_netcat():
+        os.system("python")
+
+
+        
     
 if __name__ == '__main__':
     menu()
